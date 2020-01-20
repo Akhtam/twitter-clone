11726379d04e869e68225b62fb46d9bd17df920c
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-19 16:11:45 -0800

    add routes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
+
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose
-	.connect(db, { useNewUrlParser: true })
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('Connected to MongoDB successfully'))
 	.catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello Js'));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-

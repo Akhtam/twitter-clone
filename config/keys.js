efc2db3d886cbbeb2454f05270142bcf97d7dbaa
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-19 23:07:33 -0800

    /

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	mongoURI:
-		'mongodb+srv://ak:5GootsW5MFMWOo4c@cluster0-gzcbf.mongodb.net/test?retryWrites=true&w=majority',
-	secretOrKey: ']TPGa!-)7KPC*G7?X&AZ5Q;bKj!$#?'
-	//Make sure this is your own unique string
-};

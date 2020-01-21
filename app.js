567956411fb79f5b1a88ebb3d53eaa60c427ace4
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-21 00:21:27 -0800

    fix production server

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
+
 const db = require('./config/keys').mongoURI;
 const app = express();
 
@@ -25,6 +26,10 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
 
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('frontend/build'));
 	app.get('/', (req, res) => {
@@ -33,7 +38,3 @@ if (process.env.NODE_ENV === 'production') {
 		);
 	});
 }
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));

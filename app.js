7dcb9d82a8334cb8e14e0afef5ded8646d5939cd
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-21 00:47:54 -0800

    .

--- app.js
+++ app.js
@@ -22,14 +22,7 @@ mongoose
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('frontend/build'));
-	app.get('/', (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname, 'frontend', 'build', 'index.html')
-		);
-	});
-}
+
 
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);

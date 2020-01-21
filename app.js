cc69ab45458b7e5cbf8d58f31e1e6215ab40c24a
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-21 00:55:01 -0800

    t

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const path = require('path');
 
 const db = require('./config/keys').mongoURI;
 const app = express();
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('frontend/build'));
+	app.get('/', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, 'frontend', 'build', 'index.html')
+		);
+	});
+}
 
 
 
